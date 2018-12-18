c0a92088dc4dd62a99178ec16c3931c00c0412f7
GanZiB <610699755@qq.com>
2018-12-18 10:52:38 +0800

    提交

---
+++ src/main/java/com/ganzib/shirodemo/ShiroDemoApplication.java
@@ -0,0 +1,14 @@
+package com.ganzib.shirodemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiroDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShiroDemoApplication.class, args);
+    }
+
+}
+
